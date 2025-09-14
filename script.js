6b5203a97fd41abee5fe3763d5a81546a498f08d
cspagzen <cspag.zen@gmail.com>
2025-09-14 04:05:13 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -12235,7 +12235,7 @@ function toggleHealthEditMode(teamName) {
         button.innerHTML = `
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
-                <path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z"/>
+                <path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a .5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z"/>
             </svg>
             Edit
         `;
@@ -12251,15 +12251,8 @@ function toggleHealthEditMode(teamName) {
             initializeUtilizationChart(teamData.jira?.utilization || 0);
         }, 100);
     } else {
-        // Enter edit mode - show form controls  
-        button.innerHTML = `
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <rect width="18" height="18" x="3" y="3" rx="2" ry="2"/>
-                <path d="m15 9-6 6"/>
-                <path d="m9 9 6 6"/>
-            </svg>
-            Cancel
-        `;
+        // Enter edit mode - hide edit button completely
+        button.style.display = 'none';
         container.innerHTML = renderHealthDimensionsGrid(teamData, true);
         utilizationContainer.innerHTML = renderUtilizationEditor(teamData);
     }
