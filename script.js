d2f61fe7291c4c497fc765435b7cab14872f611a
cspagzen <cspag.zen@gmail.com>
2025-09-30 23:29:26 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -2343,15 +2343,16 @@ function updatePipelineCard() {
             </div>
         </div>
         <button class="pipeline-prioritize-icon-btn" 
-                onclick="event.stopPropagation(); openQuickPrioritizeModal(boardData.bullpen.find(init => init && init.id === ${initiative.id}));"
-                title="Prioritize this initiative">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <path d="m3 8 4-4 4 4"/>
-                <path d="M7 4v16"/>
-                <path d="M11 12h10"/>
-                <path d="m17 16 4-4-4-4"/>
-            </svg>
-        </button>
+        onclick="event.stopPropagation(); openQuickPrioritizeModal(boardData.bullpen.find(init => init && init.id === ${initiative.id}));"
+        title="Prioritize this initiative">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-narrow-wide-icon lucide-arrow-up-narrow-wide">
+        <path d="m3 8 4-4 4 4"/>
+        <path d="M7 4v16"/>
+        <path d="M11 12h4"/>
+        <path d="M11 16h7"/>
+        <path d="M11 20h10"/>
+    </svg>
+</button>
     </div>
 </div>
     </div>
