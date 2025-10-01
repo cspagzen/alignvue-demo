ba3ff1c8fca872c008a0ccc1a16c14902ee8b0d5
cspagzen <cspag.zen@gmail.com>
2025-09-30 21:40:49 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -2342,11 +2342,17 @@ function updatePipelineCard() {
                         <span class="bento-validation-text">${getValidationText(initiative.validation)}</span>
                     </div>
                 </div>
-                <button class="pipeline-prioritize-btn" 
-                        onclick="event.stopPropagation(); openQuickPrioritizeModal(boardData.bullpen.find(init => init && init.id === ${initiative.id}));"
-                        title="Prioritize this initiative">
-                    Prioritize
-                </button>
+                <button class="pipeline-prioritize-icon-btn" 
+        onclick="event.stopPropagation(); openQuickPrioritizeModal(boardData.bullpen.find(init => init && init.id === ${initiative.id}));"
+        title="Prioritize this initiative">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <path d="m3 8 4-4 4 4"/>
+        <path d="M7 4v16"/>
+        <path d="M11 12h4"/>
+        <path d="M11 16h7"/>
+        <path d="M11 20h10"/>
+    </svg>
+</button>
             </div>
         </div>
     </div>
