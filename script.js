c26ab67a365f526de37c3ffb1651281b0eafd757
cspagzen <cspag.zen@gmail.com>
2025-10-01 23:10:45 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -8514,20 +8514,7 @@ function getHealthStatusTextColor(healthStatus) {
     return colors[healthStatus] || 'text-gray-700';
 }
 
-function getHealthIndicatorIcon(status) {
-    if (status === 'healthy') {
-        return `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="var(--accent-green)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="m14.479 19.374-.971.939a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5a5.2 5.2 0 0 1-.219 1.49"/>
-            <path d="M15 15h6"/>
-            <path d="M18 12v6"/>
-        </svg>`;
-    } else {
-        return `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="var(--accent-red)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="m14.876 18.99-1.368 1.323a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5a5.2 5.2 0 0 1-.244 1.572"/>
-            <path d="M15 15h6"/>
-        </svg>`;
-    }
-}
+
 
 function getTeamNotes(teamName, teamData) {
     const notes = [];
